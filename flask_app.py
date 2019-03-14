f6a8ed3725681b3ed22eaaefc0a3d0dcd85d1f4d
rapsac1 <casparga@gmx.de>
2019-03-14 17:05:32 +0100

    first working version

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,27 @@
 
-# A very simple Flask Hello World app for you to get started with...
+# A very simple Flask app
+from flask import Flask, redirect, render_template, request, url_for
+from fastai.text import *
+from pathlib import Path
+import os
 
-from flask import Flask
-from flask import render_template
+#init code
+
+dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+learner = load_learner(dir_path / 'model' ,fname='production_model.pkl')
+
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
+
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    start_text = request.form["contents"]
+    text = learner.predict(start_text, 100, temperature=1.3, min_p=0.001)
+    text = text.replace('xxbos ', '')
+    return render_template("index.html", urteil=text)
